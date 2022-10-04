eeea7e544ccd69a16162e12eff7dc19c26f5dce6
Saiviraj <91192351+Saiviraj@users.noreply.github.com>
2022-10-04 11:02:54 +0530

    Create refills.java

---
+++ refills.java
@@ -0,0 +1,524 @@
+<body>
+<h1>GREETINGS </h1>
+<input type = "button">
+<input type = "file">
+<input type ="radio">
+<iframe src = "19BCE7306_LAB_8-converted.docx"></iframe>
+<img src="www.vitap.ac.in">
+<video>
+    <audio>
+        
+    </audio>
+</video>
+<button:"click">
+
+</button:>
+<style>
+    h1{
+        color: red;
+        background-color: yellow;
+        padding: 60px;
+        margin: 20px;
+        transform-style: flat;
+        page-break-after: always;
+        row-gap: 5px;
+        transition-delay: 5s;
+        ruby-position: after;
+        height: 100px;
+        weight: bold;
+        scroll-margin:auto;
+       font-size: larger;
+       letter-spacing: 5px;
+       outline-color: blue;  
+       transform:rotate(1deg) ;  
+       border-style: dashed;
+       font-weight: bold;
+       background-image: -moz-linear-gradient(90);
+       visibility:inherit;
+       font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+       align-content: center;
+       text-align: center;
+       border-radius: 90px;
+       display: flow-root;
+       column-gap: 9px;
+       scroll-padding-inline: initial;
+       background-position: bottom;
+       lighting-color: blue;
+       max-lines: 7;
+       grid-template-areas: 3;
+       grid-row: auto;
+       justify-content:left;
+       font-feature-settings: "abvf";
+       
+       
+    }
+</style>
+<script type = "text/javascript">
+
+    function PLR() {
+    
+        var choice = document.getElementById("choice").value;
+    
+    if(choice === "Select"){
+        document.getElementById("result").innerHTML = " Please Choose an option above" ;
+    }
+    
+    if(choice === "PHYTHON"){
+        document.getElementById("result").innerHTML = " Just start with PYTHON  https://www.online-python.com" ;
+    }
+    if(choice === "Just_for_Fun"){
+    
+    document.getElementById("result").innerHTML = " Start learning LOLCODE and OMGROFL. ";
+    
+    }
+    if(choice === "Web_Development"){
+    
+    document.getElementById("result").innerHTML = " Begin with HTML CSS JAVASCRIPT and develop some web apps.";
+    
+    }
+    if(choice === "Job"){
+    
+    document.getElementById("result").innerHTML = " Start with C programming Language and continue with JAVA or PYTHON. ";
+    
+    }
+    if(choice === "Software_Development"){
+    
+    document.getElementById("result").innerHTML = " Start with JAVA and dive deep into JAVA. ";
+    
+    }
+    if(choice === "Beginners"){
+    
+    document.getElementById("result").innerHTML = " Start with PYTHON. ";
+    }
+    
+    if(choice === "Hacking"){
+    
+    document.getElementById("result").innerHTML = " Start with PYTHON and C programming and some control over OS-Linux. ";
+    }
+    
+    if(choice === "App_Development"){
+    
+    document.getElementById("result").innerHTML = " Start learning JAVA from basics for Android Devlopment. ";
+    }
+    
+    if(choice === "Cyber_Security"){
+    
+    document.getElementById("result").innerHTML = " For a career in Cyber Security, You need to learn PYTHON, C++, JAVASCRIPT, SQL. ";
+    }
+    
+    if(choice === "Game_Development"){
+    
+    document.getElementById("result").innerHTML = " Start with C++ or JAVA and you may even use PYTHON libraries. ";
+    }
+    
+    if(choice === "Data_Analytics"){
+    
+    document.getElementById("result").innerHTML = " Start with PYTHON and R programming. ";
+    }
+    
+    if(choice === "IOT"){
+    
+    document.getElementById("result").innerHTML = " Start with C programming or PYTHON to make an IOT project. ";
+    }
+    
+    if(choice === "Artificial_Intelligence"){
+    
+    document.getElementById("result").innerHTML = " Start with PYTHON which is the most popular choice followed by C++, JAVA, JAVASCRIPT, and C#. ";
+    }
+    
+    if(choice === "Animations"){
+    
+    document.getElementById("result").innerHTML = " Start with R programming and try MATLAB and PYTHON for Data Visualizations. ";
+    }
+    
+    if(choice === "Logics"){
+    
+    document.getElementById("result").innerHTML = " Start with C++ ";
+    }
+    
+    if(choice === "Kids"){
+    
+    document.getElementById("result").innerHTML = " Start with Ruby and Scratch or JAVASCRIPT. ";
+    }
+    
+    if(choice === "Future"){
+    
+    document.getElementById("result").innerHTML = " Start with PYTHON. It undoubtedly tops the list. ";
+    }
+    
+    if(choice === "Deep_Learning"){
+    
+    document.getElementById("result").innerHTML = " Start with only PYTHON and just dive deep into it's applications and libraries. ";
+    }
+    
+    if(choice === "Earn_Money"){
+    
+    document.getElementById("result").innerHTML = " Start learning GO or Ruby on Rails. ";
+    }
+    
+    if(choice === "Random"){
+    
+    document.getElementById("result").innerHTML = " Start with JAVASCRIPT. ";
+    }
+    
+    }
+    </script>
+    
+    </head>
+    
+    <body>
+    <h1 style="text-align: center;";><u>Programming Language Recommender</u></h1><br>
+    <h3>About Tool :</h3>
+    <p style="font-size:20px";>This tool will help you to choose the best programming language that suits you, If you are struck in your life or starting as a beginner, this web tool is perfect for you. Programming has become a vital element in every CSE Student's Life. When you want to start your career as a Web Developer or Software Developer or an Ethical Hacker, but you don't know how to choose, We are here for you. Just simply follow the instructions.</p>
+    
+    <p>&nbsp;</p>
+    <p><img src="https://fossbytes.com/wp-content/uploads/2016/06/most-important-programming-languages.jpg" width="480px" height="320px" alt="important">&nbsp;&nbsp;&nbsp;&nbsp;<img src="https://bbvaopen4u.com/sites/default/files/img/new/recursosprogramadores.png" width="480px" height="320px" alt="important">&nbsp;&nbsp;&nbsp;&nbsp;<img src="https://www.purelogics.net/blog/wp-content/uploads/2017/06/AAEAAQAAAAAAAAieAAAAJDE0MGRjMmY4LTY5MTEtNDk4Ny04NGY1LTY4YTFjYjkzYzQ1NQ.jpg" width="480px" height="320px" alt="Programming" ></p>
+    <p>&nbsp;</p>
+    <h3>How to use this Tool ?</h3>
+    <ol style="font-size:20px";>
+        <li>Explore different fields in Computer Science.</li>
+        <li>Come back again to this website and think to choose an option listed below.</li>
+        <li>Choose the best option that suits your goal. </li>
+        <li>Now, Click on Continue..</li>
+        <li>Your result is displayed below Recommender : </li>
+        <li>If you like this, share this link to your friends...</li>
+    </ol>
+    <br>
+    <h2>Why do you want to learn Programming ?</h2>
+    
+    <p style="color:blue;font-size:30px; font-family: fantasy; font-weight: bolder;" >Choose :</p> &nbsp;<select id = "choice">
+        <option value = "Select">Select</option>
+        <option value = "PHYTHON">PHYTHON</option>
+        <option value = "Just_for_Fun">Just for Fun</option>
+        <option value = "Web_Development">Web Development</option>
+        <option value = "Job">Job in Companies</option>
+        <option value = "Software_Development">Software Development</option>
+        <option value = "Beginners">Beginners</option>
+        <option value = "Hacking">Hacking</option>
+        <option value = "App_Development">App Development</option>
+        <option value = "Cyber_Security">Cyber Security</option>
+        <option value = "Game_Development">Game Development</option>
+        <option value = "Data_Analytics">Data Analytics</option>
+        <option value = "Artificial_Intelligence">Artificial Intelligence</option>
+        <option value = "Animations">Animations and Visualizations</option>
+        <option value = "IOT">Do an IOT Project</option>
+        <option value = "Logics">Improve your Logics</option>
+        <option value = "Kids">Kids</option>
+        <option value = "Future">Future</option>
+        <option value = "Deep_Learning">Deep Learning</option>
+        <option value = "Earn_Money">Earn Money</option>
+        <option value = "Random">Random</option>
+    </select>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    
+    <button onclick="PLR()" >Continue</button><br>
+    <p style="color:green;font-size:20px; font-weight: bold;" ><u>Recommender</u> :</p>
+    <br>
+    <h3 id="result" style=" font-size:26px; font-family: sans-serif; font-weight: bolder;"></h3><br>
+    
+    <p style="font-size:18px; font-weight: lighter;">Thank you for choosing our Tool.  All the best !</p><br>
+    <h4 style="color:#FE045F;" >This page is contributed by &copy; <a href="https://github.com/GudiVaraprasad" target="_blank">GVP</a></h4>
+    </body>
+    </html>
+    
+</body>
+<h2 id = "LOGIN"></h2>
+
+
+<script type = "text/javascript"> a</script>
+<script type = "text/javascript">b</script>
+<script type = "text-decoration">v</script>
+<a href = "19BCE7664 mad 9.pdf"></a>
+<input type ="htts" value = "login">
+<script>
+    function value();
+    let a =90;
+    document.write(a);
+
+    document.getElementById("value").innerHTML = "sandeep";
+    <button type = "details"></button>
+    if(choice == value)
+    {
+        document.getElementById("sandeep").innerHTML = "frd"
+    }
+    if(choice == value1)
+    {
+              document.getElementById("abhiraj").innerHTML ="frd";
+    }
+    <opt value = "hrml">select</opt>
+    console.log(value);
+    if(choice == parseFloat){
+        document.getElementById("sandeep1").innerHTML ="value";
+    }
+
+    
+</script>
+<style>
+    .value{
+        color:red;
+        background-color: yellow;
+        padding: 60px;
+        margin: 20px;
+        transform-style: flat;
+        page-break-after: always;
+        row-gap: 5px; 
+    }
+</style>
+<style>
+    .button{
+        color:red;
+        background-color: yellow;
+        padding: 60px;
+        margin: 20px;
+        transform-style: flat;
+        page-break-after: always;
+        row-gap: 5px;
+    }
+</style>
+
+<style>
+    .value{
+        color:red;
+        background-color: yellow;
+        padding: 60px;
+        margin: 20px;
+        transform-style: flat;
+        page-break-after: always;
+        row-gap: 6pc;
+        transition-delay: 5s;
+        ruby-position: after;
+        height: 100px;
+        scroll-margin:auto;
+       font-size: larger;
+       letter-spacing: 5px;
+       outline-color: blue;  
+       transform:rotate(1deg) ;  
+       border-style: dashed;
+       font-weight: bold;
+       background-image: -moz-linear-gradient(90);
+       visibility:inherit;
+       font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+       align-content: center;
+       text-align: center;
+       border-radius: 90px;
+       display: flow-root;
+       column-gap: 9px;
+       scroll-padding-inline: initial;
+       background-position: bottom;
+       lighting-color: blue;
+       max-lines: 7;
+       grid-template-areas: 3;
+       grid-row: auto;
+       justify-content:left;
+       font-feature-settings: "abvf";
+       
+
+    }
+</style>
+
+
+</script>
+
+<body>
+    <h1> what is sandeep  he is my frd</h1>
+
+    <div class id = "1">1</div>
+    <div class id = "2">2</div>
+    <div class id = "3">3</div>
+    <div class id = "4">4</div>
+    <div class id = "5">5</div>
+    <div class id = "6">6</div>
+    <div class id = "7">7</div>
+    <div class id = "8">8</div>
+    <div class id = "9">9</div>
+    <div class id = "10">10</div>
+    <table>
+        <tr>1</tr>
+        <tr>10</tr>
+        <tr>100</tr>
+        <tr>1000</tr>
+        <tr>199</tr>
+        <tr>1</tr>
+        <tr>1</tr>
+        <tr>1</tr>
+        <tr>1</tr>
+        <input type = "checkbox">
+
+        <td>678905678</td>
+        <style>
+            tr{
+                color:greenyellow;
+            background-color: blue;
+            padding: 20px;
+            margin:80px;
+            padding-left: 0in;
+            text-align: center;
+            transform-style: flat;
+        page-break-after: always;
+        row-gap: 6pc;
+        transition-delay: 5s;
+        ruby-position: after;
+        height: 1000px;
+        width:1000px;
+            }
+            td{
+                color:red;
+            background-color: blue;
+            padding: 20px;
+            margin:80px;
+            padding-left: 0in;
+            text-align: center;
+            transform-style: flat;
+        page-break-after: always;
+        row-gap: 6pc;
+        transition-delay: 5s;
+        ruby-position: after;
+        height:1000px;
+        width:1000px;
+            }
+        </style>
+    </table>
+
+    <style>
+        div{
+            color:red;
+            background-color: blue;
+            padding: 20px;
+            margin:80px;
+            padding-left: 0in;
+            text-align: center;
+            transform:rotate(90degree);
+            transform-style: flat;
+        page-break-after: always;
+        row-gap: 6pc;
+        transition-delay: 5s;
+        ruby-position: after;
+        height: 100px;
+        scroll-margin:auto;
+       font-size: larger;
+       letter-spacing: 5px;
+       outline-color: blue;  
+       transform:rotate(1deg) ;  
+       border-style: dashed;
+       font-weight: bold;
+       background-image: -moz-linear-gradient(90);
+       visibility:inherit;
+       font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+       align-content: center;
+       text-align: center;
+       border-radius: 90px;
+       display: flow-root;
+       column-gap: 9px;
+       scroll-padding-inline: initial;
+       background-position: bottom;
+       lighting-color: blue;
+       max-lines: 7;
+       grid-template-areas: 3;
+       grid-row: auto;
+       justify-content:left;
+       font-feature-settings: "abvf";
+
+        }
+        h1{
+            color:red;
+            background-color: blue;
+            padding: 20px;
+            margin:80px;
+            padding-left: 0in;
+            transform:rotate(90degree);
+            transform-style: flat;
+        page-break-after: always;
+        row-gap: 6pc;
+        transition-delay: 5s;
+        ruby-position: after;
+        height: 100px;
+        scroll-margin:auto;
+       font-size: larger;
+       letter-spacing: 4px;
+       outline-color: blue;  
+       transform:rotate(1deg) ;  
+       border-style: dashed;
+       font-weight: bold;
+       background-image: -moz-linear-gradient(90);
+       visibility:inherit;
+       font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+       align-content: center;
+       text-align: center;
+       border-radius: 90px;
+       display: flow-root;
+       column-gap: 9px;
+       scroll-padding-inline: initial;
+       background-position: bottom;
+       lighting-color: blue;
+       max-lines: 7;
+       grid-template-areas: 3;
+       grid-row: auto;
+       justify-content:left;
+       font-feature-settings: "abvf";
+
+ 
+        }
+
+
+    </style>
+
+</body>
+<script>
+    window.alert("enter your name");
+    window.prompt("enter your name");
+
+    function myfunction();
+    {
+        
+       document.getElementById("paragraph").innerHTML ="sandeep is my frd";
+    }
+    </script>
+    <body>
+        <h1> wht is css </h1>
+        <p id = "paragraph"> sandeep to viraj</p>
+         
+        <button type = "button" onclick = "myfunction()"> try it </button>
+        </body>
+        <body>
+            <style>
+                .btn{
+                    color:blue;
+                    background: fixed;
+                    background-attachment: fixed;
+                    background-color: beige;
+                    padding: 100pc;
+                    margin: 60px;
+                    padding-left: -15px;
+                    padding-top: -90pc;
+
+
+                }
+                .btn::before,
+                .btn::after{
+                    position: sticky;
+                    width: 100px;
+                    height: 90pc;
+
+                }
+                .btn:hover::before,
+                .btn:hover::after{
+                    width:calc(25%+90px);
+                    height:1000px;
+                }
+
+            </style>
+            <a href = "#" class = "btn">more</a>
+        </body>
+        <script>
+            let c = ["hellllo","saai"];
+            console.log(c);
+        </script>
+        <style>
+            .c{
+                position: sticky;
+                    width: 100px;
+                    height: 90pc;
+  
+
+            }
+        <
+        
